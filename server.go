e0814fba88af3bd44e70a275d8cc0fdc4346cfde
Mihail Gorelikov <gorelikovMihail03@gmail.com>
2021-03-11 01:14:22 +0300

    feat: add server

---
+++ server.go
@@ -0,0 +1,22 @@
+package todo_rest_api
+
+import (
+	"net/http"
+	"time"
+)
+
+type Server struct {
+	httpServer	*http.Server
+}
+
+func (s* Server) Run(port string) error {
+	s.httpServer = &http.Server{
+		Addr: ":" + port,
+		MaxHeaderBytes: 1 << 20,
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	return s.httpServer.ListenAndServe()
+}
+
