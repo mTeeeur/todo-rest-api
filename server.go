205881ac33d595ca9e0a8c0953c0801b69b417fa
Mihail Gorelikov <gorelikovMihail03@gmail.com>
2021-03-11 01:21:06 +0300

    feat: add user

--- server.go
+++ server.go
@@ -7,21 +7,20 @@ import (
 )
 
 type Server struct {
-	httpServer	*http.Server
+	httpServer *http.Server
 }
 
-func (s* Server) Run(port string) error {
+func (s *Server) Run(port string) error {
 	s.httpServer = &http.Server{
-		Addr: ":" + port,
+		Addr:           ":" + port,
 		MaxHeaderBytes: 1 << 20,
-		ReadTimeout: 10 * time.Second,
-		WriteTimeout: 10 * time.Second,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
 	}
 
 	return s.httpServer.ListenAndServe()
 }
 
-func (s* Server) Shutdown(ctx context.Context) error {
+func (s *Server) Shutdown(ctx context.Context) error {
 	return s.httpServer.Shutdown(ctx)
 }
-
---
+++ user.go
@@ -0,0 +1,8 @@
+package todo_rest_api
+
+type User struct {
+	Id       int    `json:"-"`
+	Name     string `json:"name"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
