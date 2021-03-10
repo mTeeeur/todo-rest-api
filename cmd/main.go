40904c0efcd52f35666fc5c6f9b78d15a1e87f56
Mihail Gorelikov <gorelikovMihail03@gmail.com>
2021-03-11 01:32:10 +0300

    feat: update main

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"log"
 	todo_rest_api "todo-rest-api"
+	"todo-rest-api/pkg/handler"
 )
 
 func main() {
 	server := new(todo_rest_api.Server)
+	handlers := new(handler.Handler)
 
-	if err := server.Run("8000"); err != nil {
+	if err := server.Run("8000", handlers.Init()); err != nil {
 		log.Fatalf("Error: %s", err.Error())
 	}
 }
