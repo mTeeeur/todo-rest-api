76495d889d538b10b906cf75026d244418935391
Mihail Gorelikov <gorelikovMihail03@gmail.com>
2021-03-11 02:27:15 +0300

    feat: update main

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,15 @@ import (
 	"log"
 	todo_rest_api "todo-rest-api"
 	"todo-rest-api/pkg/handler"
+	"todo-rest-api/pkg/repository"
+	"todo-rest-api/pkg/service"
 )
 
 func main() {
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 	server := new(todo_rest_api.Server)
-	handlers := new(handler.Handler)
 
 	if err := server.Run("8000", handlers.Init()); err != nil {
 		log.Fatalf("Error: %s", err.Error())
