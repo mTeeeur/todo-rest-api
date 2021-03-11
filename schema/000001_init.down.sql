1268130936469d188fb575900c9339a2e155f35c
Mihail Gorelikov <gorelikovMihail03@gmail.com>
2021-03-11 12:55:00 +0300

    feat: update migration

--- schema/000001_init.down.sql
+++ schema/000001_init.down.sql
@@ -1,5 +1,5 @@
-DROP TABLE users;
-DROP TABLE todo_lists;
-DROP TABLE users_list;
-DROP TABLE todo_items;
 DROP TABLE list_items;
+DROP TABLE todo_items;
+DROP TABLE users_list;
+DROP TABLE todo_lists;
+DROP TABLE users;
--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -1,36 +1,36 @@
 CREATE TABLE users
 (
-    id            serializable not null unique,
+    id            serial       not null unique,
     name          varchar(255) not null,
     username      varchar(255) not null unique,
     password_hash varchar(255) not null
-)
+);
 
 CREATE TABLE todo_lists
 (
-    id          serializable not null unique,
+    id          serial       not null unique,
     title       varchar(255) not null,
     description varchar(255)
-)
+);
 
-CREATE TABLE users_list
+CREATE TABLE users_lists
 (
-    id      serializable                                     not null unique,
+    id      serial                                           not null unique,
     user_id int references users (id) on delete cascade      not null,
-    list_id int references todo_lists (id) on delete cascade not null,
-)
+    list_id int references todo_lists (id) on delete cascade not null
+);
 
 CREATE TABLE todo_items
 (
-    id          serializable not null unique,
+    id          serial       not null unique,
     title       varchar(255) not null,
     description varchar(255),
-    compilted   Boolean null default false
-)
+    done        boolean      not null default false
+);
 
-CREATE TABLE list_items
+CREATE TABLE lists_items
 (
-    id      serializable                                     not null unique,
+    id      serial                                           not null unique,
     item_id int references todo_items (id) on delete cascade not null,
     list_id int references todo_lists (id) on delete cascade not null
-)
+);
