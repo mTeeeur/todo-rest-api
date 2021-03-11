e585bf428b39a09755a48bee1808f14a6ca817a8
Mihail Gorelikov <gorelikovMihail03@gmail.com>
2021-03-11 13:16:02 +0300

    fix: minor fix

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "github.com/lib/pq"
 	todo_rest_api "github.com/mTeeeur/todo-rest-api"
 	"github.com/mTeeeur/todo-rest-api/pkg/handler"
 	"github.com/mTeeeur/todo-rest-api/pkg/repository"
