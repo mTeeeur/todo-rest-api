7df307efba27ce0f113196d39d590438b8fe963b
Mihail Gorelikov <gorelikovMihail03@gmail.com>
2021-03-14 14:44:29 +0300

    feat: add error handler

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -1,8 +1,15 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	todo_rest_api "github.com/mTeeeur/todo-rest-api"
+)
 
 func (h *Handler) login(c *gin.Context) {
+	var input todo_rest_api.User
+
+	if err := c.BindJSON(&input); err != nil {
+	}
 }
 
 func (h *Handler) register(c *gin.Context) {
