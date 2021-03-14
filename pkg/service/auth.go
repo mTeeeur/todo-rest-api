718aba02ebae0bd9e48948be2b446ce60d1c14b1
Mihail Gorelikov <gorelikovMihail03@gmail.com>
2021-03-14 14:59:09 +0300

    feat: add hash password method

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -1,9 +1,13 @@
 package service
 
 import (
+	"crypto/sha1"
+	"fmt"
 	todo_rest_api "github.com/mTeeeur/todo-rest-api"
 	"github.com/mTeeeur/todo-rest-api/pkg/repository"
 )
+
+const salt = "arozaypalanalapyazora"
 
 type AuthService struct {
 	repo repository.Authorization
@@ -14,5 +18,13 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 }
 
 func (s *AuthService) CreateUser(user todo_rest_api.User) (int, error) {
+	user.Password = s.generateHashPassword(user.Password)
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) generateHashPassword(password string) string {
+	hash := sha1.New()
+	hash.Write([]byte(password))
+
+	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
 }
